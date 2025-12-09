5ec8d2e1b5c77ff1f8aa95fbe5f6aa74ff1afcce
coco29 <tonyp8@126.com>
2025-12-09 20:19:29 +0800

    chore

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import router from './router/' //index.js 可以省略(defult)
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
